556cf56dc257faefbd688d2680a80471ab85ea59
axiom <medved_axiom@mail.ru>
2019-11-30 13:56:15 +0300

    Четвертое ДЗ

--- Lesson_4/1.py
+++ Lesson_4/1.py
@@ -3,3 +3,81 @@
 разработанных в рамках домашнего задания первых трех уроков.
 Примечание: попробуйте написать несколько реализаций алгоритма и сравнить их.
 """
+
+# Пример 2-3
+
+#3.	Сформировать из введенного числа обратное по порядку входящих в него
+#цифр и вывести на экран. Например, если введено число 3486,
+# то надо вывести число 6843.
+
+import cProfile
+import sys
+import timeit
+import time as t
+
+
+sys.setrecursionlimit(1000000)
+
+
+def timer_dec(dec_func):
+    def tmp(*arg):
+        st_time = t.time() * 1000
+        result = dec_func(*arg)
+        time = t.time() * 1000 - st_time
+        print(f"Время выполнения: {time:.9f} мс")
+        return result
+    return tmp
+
+
+#@timer_dec
+def rec_count(NUMB):
+    RES = 0
+    while NUMB > 0:
+        RES = RES * 10 + NUMB % 10
+        NUMB = NUMB // 10
+    print(RES)
+
+
+#@timer_dec
+def rec_reverse(NUMB, rev=0):
+    if NUMB == 0:
+        return rev
+    else:
+        rev = float((rev + (NUMB %10) * 0.1) * 10)
+        NUMB = NUMB // 10
+        return rec_reverse(NUMB, rev)
+
+
+
+print(f"rec_count {rec_count(34560**200)}")
+print(10 * "---")
+# Время выполнения: 1.001464844 мс
+print(f"rec_reverse {rec_reverse(34560**200)}")
+print(10 * "---")
+# Время выполнения: 1.000732422 мс
+
+
+
+cProfile.run("rec_count(34560**200)")
+# 5 function calls in 0.002 seconds
+cProfile.run("rec_reverse(34560**2000)")
+# 5243 function calls (4 primitive calls) in 0.091 seconds
+
+
+
+print(timeit.timeit("rec_count(34560**200)", number=1, setup="from __main__ import rec_count"))
+# 0.0015569
+print(timeit.timeit("rec_reverse(34560**200)", number=1, setup="from __main__ import rec_reverse"))
+# 0.0015534
+
+
+'''
+По времени работы оба кода работают примерно одинаково,
+но при профилировании видно что рекурсивная функция сильно проигрывает
+Необходима оптимизация кода.
+
+В рекурсивной функции пришлось заменить int на float. При прогоне программа не могла перевести 
+число в целое.
+'''
+
+
--- Lesson_4/2.py
+++ Lesson_4/2.py
@@ -3,3 +3,64 @@
 Без использования «Решета Эратосфена»;
 Используя алгоритм «Решето Эратосфена»
 """
+
+import timeit
+import cProfile
+
+
+def simple_num(ind):
+    LST = [2]
+    i = 3
+    while len(LST) < ind:
+        for j in LST:
+            if i % j == 0:
+                break
+        else:
+            LST.append(i)
+        i += 1
+    return LST[ind - 1]
+print(simple_num(1000))
+
+
+def eratosfen(num):
+    LST = [0] * 10000
+    for i in range(10000):
+        LST[i] = i
+    LST[1] = 0
+    IND = 2
+    while IND < 10000:
+        if LST[IND] != 0:
+            j = IND * 2
+            while j < 10000:
+                LST[j] = 0
+                j = j + IND
+        IND += 1
+    fin_lst = [el for el in LST if el != 0]
+    return fin_lst[num - 1]
+print(eratosfen(1000))
+
+#cProfile.run("simple_num(10)")
+# 18 function calls in 0.000 seconds
+#cProfile.run("eratosfen(10)")
+# 5 function calls in 0.006 seconds
+
+
+#cProfile.run("simple_num(1000)")
+# 8921 function calls in 0.038 seconds
+#cProfile.run("eratosfen(1000)")
+# 5 function calls in 0.006 seconds
+
+#print(timeit.timeit("simple_num(5)", number=1, setup="from __main__ import simple_num"))
+# 0.0002145
+#print(timeit.timeit("eratosfen(5)", number=1, setup="from __main__ import eratosfen"))
+# 2.1452147
+
+#print(timeit.timeit("simple_num(1000)", number=1, setup="from __main__ import simple_num"))
+# 0.0021322
+#print(timeit.timeit("eratosfen(1000)", number=1, setup="from __main__ import eratosfen"))
+# 0.006143599999999999
+
+'''
+Решение с использованием решетки Эратосфена проигрывает на малых вводных данных,
+но показывает лучшие результаты при вычеслении больших данных
+'''

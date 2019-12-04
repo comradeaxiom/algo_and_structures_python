183b53afcfa80ffd1ef37573ca328e22ac494c20
axiom <medved_axiom@mail.ru>
2019-12-04 12:25:22 +0300

    Пятое ДЗ

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,34 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+
+from collections import namedtuple
+
+
+n = int(input("Колличество предприятий: "))
+COMPANIES = namedtuple("company", "name kv_01 kv_02 kv_03 kv_04")
+PROFIT = {}
+
+for i in range(n):
+    Company = COMPANIES(
+        name=input("Название предприятия: "),
+        kv_01=int(input("Выручка за 1-й квартал: ")),
+        kv_02=int(input("Выручка за 2-й квартал: ")),
+        kv_03=int(input("Выручка за 3-й квартал: ")),
+        kv_04=int(input("Выручка за 4-й квартал: "))
+    )
+    PROFIT[Company.name] = int(Company.kv_01 + Company.kv_02 + Company.kv_03 + Company.kv_04)
+
+TOTAL = 0
+for value in PROFIT.values():
+    TOTAL += value
+TOTAL = TOTAL / n
+
+
+for key, value in PROFIT.items():
+    if value > TOTAL:
+        print(key, " - прибыль выше среднего")
+    if value < TOTAL:
+        print(key, " - прибыль ниже среднего")
+    if value == TOTAL:
+        print(key, " - средняя прибыль")
--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,27 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+
+from collections import deque
+
+NUM_1 = deque(input("Введите первое число: "))
+NUM_2 = deque(input("Введите второе число: "))
+
+
+def hex_sum(NUM_1, NUM_2):
+    NUM_1 = "".join([i for i in NUM_1])
+    NUM_2 = "".join([i for i in NUM_2])
+    SUM = hex((int(float.fromhex(NUM_1) + float.fromhex(NUM_2))))
+    SUM = deque(SUM[2::].upper())
+    print("Сумма: ", SUM)
+
+
+def hex_mul(NUM_1, NUM_2):
+    NUM_1 = "".join([i for i in NUM_1])
+    NUM_2 = "".join([i for i in NUM_2])
+    SUM = hex((int(float.fromhex(NUM_1) * float.fromhex(NUM_2))))
+    SUM = deque(SUM[2::].upper())
+    print("Произведение: ", SUM)
+
+hex_sum(NUM_1, NUM_2)
+hex_mul(NUM_1, NUM_2)
